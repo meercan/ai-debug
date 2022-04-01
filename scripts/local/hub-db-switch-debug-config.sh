8337e2bf3392714b132e4e69144641a998b0bf90
Ege Ercan <eercan@veritone.com>
2022-04-01 10:36:36 -0400

    Config switch

--- scripts/local/hub-db-switch-debug-config.sh
+++ scripts/local/hub-db-switch-debug-config.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 echo "Switching to debug config - Inserting debug env configs in hub.config"
+
+truncate_script_file=hub-db-truncate-config.sql
 sql_script_file=hub-db-debug-config.sql
+
 docker cp "./scripts/sql/${sql_script_file}" ai-dev-postgres:/
+docker cp "./scripts/sql/${truncate_script_file}" ai-dev-postgres:/
+docker exec -i ai-dev-postgres psql -U postgres -d postgres -f "./${truncate_script_file}"
 docker exec -i ai-dev-postgres psql -U postgres -d postgres -f "./${sql_script_file}"
--- scripts/local/hub-db-switch-dev-config.sh
+++ scripts/local/hub-db-switch-dev-config.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 echo "Switching to dev config - Inserting dev env configs in hub.config"
+truncate_script_file=hub-db-truncate-config.sql
 sql_script_file=hub-db-dev-config.sql
+
 docker cp "./scripts/sql/${sql_script_file}" ai-dev-postgres:/
+docker cp "./scripts/sql/${truncate_script_file}" ai-dev-postgres:/
+docker exec -i ai-dev-postgres psql -U postgres -d postgres -f "./${truncate_script_file}"
 docker exec -i ai-dev-postgres psql -U postgres -d postgres -f ./"${sql_script_file}"
