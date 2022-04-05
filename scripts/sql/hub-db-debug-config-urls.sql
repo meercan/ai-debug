30e6bb88ee0042b8215beb368ce0290df3576b8a
Ege Ercan <eercan@veritone.com>
2022-04-05 14:35:30 -0400

    delete

---
+++ scripts/sql/hub-db-debug-config-urls.sql
@@ -0,0 +1,2 @@
+INSERT INTO hub.config (config_section, config_key, config_value, kvp, created_date_time, modified_date_time) VALUES('admin'::hub."type_config_section", 'admin.hub.domain_name', 'hub-server--session-ege.ngrok.io', '{}'::jsonb, 1625749456, 1640101139);
+INSERT INTO hub.config (config_section, config_key, config_value, kvp, created_date_time, modified_date_time) VALUES('admin'::hub."type_config_section", 'admin.aiware.get_aiware_domain', 'file-server--session-ege.ngrok.io/hub', '{}'::jsonb, 1640098932, 1640100785);
